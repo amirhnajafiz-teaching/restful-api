851e65f570ba4caa15e7e73c65655201b6da9cb9
9831065 <najafizadeh21@gmail.com>
2021-09-23 19:37:42 +0330

    Front card refactor

--- resources/views/weather.card.blade.php
+++ resources/views/weather.card.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
     <div class="card">
-        <h2>
-            {{ $response['name'] }}
-        </h2>
-        <h3>
-            {{ $response['weather']['main'] }}
-            <span>Wind {{ $response['wind']['speed'] }}</span>
+        <h2>{{ $location['name'] }} <img src="https://www.countryflags.io/{{ $location['country'] }}/flat/64.png"
+                                         alt="{{ $location['country'] }}"></h2>
+        <h3>{{ $weather['current']['weather'][0]['main'] }}<span>Wind {{ $weather['current']['wind_speed'] }}km/h</span>
         </h3>
-        <h1>{{ $response['main']['temp'] }}°</h1>
+        <h1>{{ round($weather['current']['temp']) }}°</h1>
         <div class="sky">
-            <div class="sun"></div>
-            <div class="cloud">
-                <div class="circle-small"></div>
-                <div class="circle-tall"></div>
-                <div class="circle-medium"></div>
-            </div>
+            <img src="https://openweathermap.org/img/wn/{{ $weather['current']['weather'][0]['icon'] }}@2x.png"
+                 alt="{{ $weather['current']['weather'][0]['description'] }}">
         </div>
+        <table>
+            <tr>
+                @foreach(range(0, count($weather['daily']) - 1) as $i)
+                    <td>{{ now()->addDays($i)->format('D') }}</td>
+                @endforeach
+            </tr>
+            <tr>
+                @foreach($weather['daily'] as $day)
+                    <td>{{ round($day['temp']['max']) }}°</td>
+                @endforeach
+            </tr>
+            <tr>
+                @foreach($weather['daily'] as $day)
+                    <td>{{ round($day['temp']['min']) }}°</td>
+                @endforeach
+            </tr>
+        </table>
     </div>
 @stop
