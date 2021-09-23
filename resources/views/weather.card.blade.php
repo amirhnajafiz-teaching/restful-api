76f517a312e35071ffed60108bd27f61ccb28e56
9831065 <najafizadeh21@gmail.com>
2021-09-23 16:02:36 +0330

    Weather card view dynamic data added

--- resources/views/weather.card.blade.php
+++ resources/views/weather.card.blade.php
@@ -1,7 +1,12 @@
 <div class="card">
-    <h2>Brussels</h2>
-    <h3>Cloudy<span>Wind 10km/h <span class="dot">•</span> Precip 0%</span></h3>
-    <h1>23°</h1>
+    <h2>
+        {{ $response['name'] }}
+    </h2>
+    <h3>
+        {{ $response['weather']['main'] }}
+        <span>Wind {{ $response['wind']['speed'] }}</span>
+    </h3>
+    <h1>{{ $response['main']['temp'] }}°</h1>
     <div class="sky">
         <div class="sun"></div>
         <div class="cloud">
@@ -10,27 +15,4 @@
             <div class="circle-medium"></div>
         </div>
     </div>
-    <table>
-        <tr>
-            <td>TUE</td>
-            <td>WED</td>
-            <td>THU</td>
-            <td>FRI</td>
-            <td>SAT</td>
-        </tr>
-        <tr>
-            <td>30°</td>
-            <td>34°</td>
-            <td>36°</td>
-            <td>34°</td>
-            <td>37°</td>
-        </tr>
-        <tr>
-            <td>17°</td>
-            <td>22°</td>
-            <td>19°</td>
-            <td>23°</td>
-            <td>19°</td>
-        </tr>
-    </table>
 </div>
