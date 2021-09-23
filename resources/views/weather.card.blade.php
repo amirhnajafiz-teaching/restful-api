7efac1a2580963be7321efa64dec1ffd3d44907e
9831065 <najafizadeh21@gmail.com>
2021-09-23 16:08:41 +0330

    Front fixed

--- resources/views/weather.card.blade.php
+++ resources/views/weather.card.blade.php
@@ -1,18 +1,22 @@
-<div class="card">
-    <h2>
-        {{ $response['name'] }}
-    </h2>
-    <h3>
-        {{ $response['weather']['main'] }}
-        <span>Wind {{ $response['wind']['speed'] }}</span>
-    </h3>
-    <h1>{{ $response['main']['temp'] }}°</h1>
-    <div class="sky">
-        <div class="sun"></div>
-        <div class="cloud">
-            <div class="circle-small"></div>
-            <div class="circle-tall"></div>
-            <div class="circle-medium"></div>
+@extends('welcome')
+
+@section('content')
+    <div class="card">
+        <h2>
+            {{ $response['name'] }}
+        </h2>
+        <h3>
+            {{ $response['weather']['main'] }}
+            <span>Wind {{ $response['wind']['speed'] }}</span>
+        </h3>
+        <h1>{{ $response['main']['temp'] }}°</h1>
+        <div class="sky">
+            <div class="sun"></div>
+            <div class="cloud">
+                <div class="circle-small"></div>
+                <div class="circle-tall"></div>
+                <div class="circle-medium"></div>
+            </div>
         </div>
     </div>
-</div>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <!-- My external CSS style -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+
+    <title>
+        Weather Channel
+    </title>
 </head>
 <body>
-
+<div>
+    <h2>
+        Hi, Welcome to Weather Channel
+    </h2>
     <form action="{{ route('channel.index') }}" method="post">
         @csrf
         <label for="city">
             Enter the city name
         </label>
-        <input type="text" id="city" name="city" />
+        <input type="text" id="city" name="city"/>
         <button type="submit">
             Get weather
         </button>
     </form>
-
+</div>
+<div>
+    @yield('content')
+</div>
 </body>
 </html>
