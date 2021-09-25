90a11a01e4f965fe95eae7887e2d9b92f6423b5f
9831065 <najafizadeh21@gmail.com>
2021-09-25 11:16:55 +0330

    Error handling

--- weather-channel/app/Http/Controllers/MainController.php
+++ weather-channel/app/Http/Controllers/MainController.php
@@ -7,6 +7,10 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Mockery\Exception;
+use PHPUnit\Framework\Error;
+use Psy\Exception\ErrorException;
+use function PHPUnit\Framework\isNull;
 
 /**
  * Class MainController.
@@ -28,19 +32,23 @@ class MainController extends Controller
     /**
      * Handling the weather request to our WC API.
      *
-     * @param string $city
      * @param Request $request
-     * @return Application|Factory|View
+     * @param string $city
+     * @return Factory|View|Application
      */
     public static function main(Request $request, string $city = 'tehran'): Factory|View|Application
     {
-        $city = $request->input('city', $city);
+        $city = $request->get('city', $city);
 
         $location = Http::get('https://api.openweathermap.org/geo/1.0/direct', [
             'appid' => '8516dbc0b619c1a5b25dc01f1ce492b1',
             'q' => $city,
             'limit' => 1
-        ])->json()[0];
+        ])->json()[0] ?? null;
+
+        if (isNull($location)) {
+            abort(404);
+        }
 
         $response = Http::get('https://api.openweathermap.org/data/2.5/onecall', [
             'appid' => '8516dbc0b619c1a5b25dc01f1ce492b1',
