efdc7809b855839e4649448110f9812aeabef4a7
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-03 13:05:43 +0330

    add: fastapi main

---
+++ fastapi/main.py
@@ -0,0 +1,13 @@
+import fastapi
+from fastapi import FastAPI
+
+
+
+# create an instance
+app = FastAPI()
+
+
+# create routes
+@app.get("/")
+def home():
+    return {"Version", fastapi.__version__}
