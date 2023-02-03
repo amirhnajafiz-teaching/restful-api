420cf0a53385267c8656edbcfcd644099183eb93
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-03 13:17:57 +0330

    add: endpoints

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,13 +1,28 @@
 import fastapi
 from fastapi import FastAPI
 
+from model import Device
 
+
+
+# create list of devices
+devices = []
 
 # create an instance
 app = FastAPI()
 
 
 # create routes
-@app.get("/")
-def home():
+@app.get("/api")
+def root():
     return {"Version": fastapi.__version__}
+
+
+@app.get("/api/device")
+def get_devices():
+    pass
+
+
+@app.post("/api/device")
+def add_device():
+    pass
